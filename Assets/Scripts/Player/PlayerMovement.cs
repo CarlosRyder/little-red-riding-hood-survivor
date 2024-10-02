51425507dc171c3df4d5c43f223f383fdc73d058
Carlos Palacios <carlos_a.riascos@uao.edu.co>
2024-10-02 16:40:08 -0500

    Update Player movement and Idle animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     #region Variables
     [SerializeField] private float speed = 1f;
-    private new Rigidbody2D rigidbody2D;
+    [SerializeField] private float raycastDistance = 1f; // Distance for the raycast
+    private Rigidbody2D rigidbody2D;
     private Vector2 moveInput;
     private Animator animator;
+
+    // Variables to store the last movement direction before stopping
+    private float lastHorizontal;
+    private float lastVertical;
     #endregion
 
     #region Start
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();   
+        animator = GetComponent<Animator>();
     }
     #endregion
 
@@ -26,16 +30,47 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
-        animator.SetFloat("Horizontal", moveX);
-        animator.SetFloat("Vertical", moveY);
+
+        // Update the speed parameter in the animator
         animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        // If there is movement, update last direction variables
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            lastHorizontal = moveX;
+            lastVertical = moveY;
+
+            // Set animator parameters for current movement
+            animator.SetFloat("Horizontal", moveX);
+            animator.SetFloat("Vertical", moveY);
+        }
+        else
+        {
+            // When speed is zero, store the last direction in the animator
+            animator.SetFloat("LastHorizontal", lastHorizontal);
+            animator.SetFloat("LastVertical", lastVertical);
+        }
+
+        // Perform a Raycast in the direction of the last movement
+        Vector2 raycastDirection = new Vector2(lastHorizontal, lastVertical).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(rigidbody2D.position, raycastDirection, raycastDistance);
+
+        // Draw the ray in the scene view for debugging purposes
+        Debug.DrawRay(rigidbody2D.position, raycastDirection * raycastDistance, Color.red);
+
+        // If the ray hits something
+        if (hit.collider != null)
+        {
+            Debug.Log("Raycast hit: " + hit.collider.name);
+        }
     }
     #endregion
 
     #region FixedUpdate
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rigidbody2D.MovePosition(rigidbody2D.position +  moveInput * speed * Time.fixedDeltaTime);
+        // Move the player
+        rigidbody2D.MovePosition(rigidbody2D.position + moveInput * speed * Time.fixedDeltaTime);
     }
     #endregion
 }
